ea6dbf64e462b2056069c55d448afbba14480031
GonzaCardoso <88778623+GonzaCardoso@users.noreply.github.com>
2023-05-31 10:40:34 -0300

    solucion 1

--- ejercicios/ej01.go
+++ ejercicios/ej01.go
@@ -5,7 +5,24 @@ package ejercicios
 // los cuenta y después genera el arreglo final con los elementos ordenados.
 
 func Ej1(arr []int) {
-	panic("No implementado")
+		if len(arr) <= 1 {
+		return
+	}
+	buckets := make([]int, 10, 10)
+
+	// Colocar los elementos en los baldes correspondientes
+	for _, num := range arr {
+		buckets[num]++
+	}
+
+	// Combinar los buckets en el arreglo original
+	pos := 0
+	for value, i := range buckets {
+		for j := 0; j < i; j++ {
+			arr[pos] = value
+			pos++
+		}
+	}
 }
 
 // Escribir tests unitarios para la función Ej1
